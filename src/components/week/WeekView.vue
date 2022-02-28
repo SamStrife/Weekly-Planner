5698db4c2fc9fb868b409402ece81d1c0a55fa17
Sam <sam.a.fletcher@gmail.com>
2022-02-28 19:26:05 +0000

    Merged into one component to avoid conflict

--- src/components/week/WeekView.vue
+++ src/components/week/WeekView.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="page" v-shadow="2">
     <div class="cal" v-shadow="1">
-      <Fullcalendar></Fullcalendar>
+      <p v-for="event in eventsImport">{{ event }}</p>
+      <FullCalendar :options="calendarOptions" />
     </div>
     <div class="under">
       <div class="considerations" v-shadow="1">
@@ -15,7 +16,38 @@
 </template>
 
 <script setup>
-import Fullcalendar from '../week/Calendar.vue';
+import '@fullcalendar/core/vdom'; // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3';
+import timeGrid from '@fullcalendar/timegrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { useStore } from 'vuex';
+import { computed } from 'vue';
+
+const store = useStore();
+store.dispatch('getEvents');
+
+const eventsImport = computed(() => store.getters.events);
+console.log(eventsImport);
+const events = [];
+
+eventsImport.value.forEach((event) => events.push({ event }));
+
+console.log(events);
+
+const calendarOptions = {
+  plugins: [timeGrid, interactionPlugin],
+  initialView: 'timeGridWeek',
+  slotMinTime: '07:00:00',
+  slotMaxTime: '18:00:00',
+  weekends: false,
+  locale: 'en-GB',
+  contentHeight: 'auto',
+  height: 'auto',
+  editable: true,
+  eventStartEditable: true,
+  selectable: true,
+  events: eventsImport,
+};
 </script>
 
 <style scoped>
