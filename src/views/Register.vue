2f79cd4cbacd2b5f156120449b884d6b6bb2be0d
Sam Fletcher <sam.a.fletcher@gmail.com>
2022-03-08 15:24:17 +0000

    Added password validation and logic to register page

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -36,6 +36,17 @@
         ></ui-textfield>
       </ui-form-field>
       <ui-form-field>
+        <ui-textfield
+          outlined
+          input-type="password"
+          placeholder="Confirm Password"
+          required
+          v-model="confirmedPassword"
+          v-on:blur="checkPasswords"
+        ></ui-textfield>
+        <p class="error" v-if="!passwordMatch">Passwords do not match</p>
+      </ui-form-field>
+      <ui-form-field>
         <base-button icon="key" @click="register">Confirm</base-button>
       </ui-form-field>
     </ui-form>
@@ -59,31 +70,41 @@ const firstName = ref('');
 const lastName = ref('');
 const email = ref('');
 const password = ref('');
+const confirmedPassword = ref('');
+const passwordMatch = ref(true);
 const uid = ref('');
 
+const checkPasswords = function () {
+  return password.value === confirmedPassword.value
+    ? (passwordMatch.value = true)
+    : (passwordMatch.value = false);
+};
+
 const register = () => {
-  createUserWithEmailAndPassword(getAuth(), email.value, password.value)
-    .then((data) => {
-      console.log('Successfully Registered!');
-      uid.value = data.user.uid;
-      setDoc(doc(membersCol, uid.value), {
-        uid: uid.value,
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
+  if (passwordMatch.value) {
+    createUserWithEmailAndPassword(getAuth(), email.value, password.value)
+      .then((data) => {
+        console.log('Successfully Registered!');
+        uid.value = data.user.uid;
+        setDoc(doc(membersCol, uid.value), {
+          uid: uid.value,
+          firstName: firstName.value,
+          lastName: lastName.value,
+          email: email.value,
+        });
+        store.dispatch('getActiveUser', {
+          id: uid.value,
+          firstName: firstName.value,
+          lastName: lastName.value,
+          email: email.value,
+        });
+        router.replace('/');
+      })
+      .catch((error) => {
+        console.log(error.code);
+        alert(error.message);
       });
-      store.dispatch('getActiveUser', {
-        id: uid.value,
-        firstName: firstName.value,
-        lastName: lastName.value,
-        email: email.value,
-      });
-      router.replace('/');
-    })
-    .catch((error) => {
-      console.log(error.code);
-      alert(error.message);
-    });
+  }
 };
 </script>
 
@@ -92,5 +113,9 @@ const register = () => {
   display: flex;
   justify-content: center;
   padding: 10px;
+}
+
+.error {
+  color: crimson;
 }
 </style>
